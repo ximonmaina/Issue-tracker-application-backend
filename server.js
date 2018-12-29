c371c8b8e143712cc956b275b05ab6a1fa0ba601
ximonmaina <simonmaina721@gmail.com>
2018-12-29 12:30:15 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+
+import cors from 'cors';
+
+import bodyParser from 'body-parser';
+
+import mongoose from 'mongoose';
+
+const app = express();
+
+const router = express.Router();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://[server]/issues');
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully!')
+});
+
+app.use('/', router);
+
+app.get('/', (req,res) => res.send('Hello World'));
+app.listen(4000, () => console.log(`Express server running on port 4000`));
+
